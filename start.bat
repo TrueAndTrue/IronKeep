e7fcca9e156b31fd00aafd02532198facf35365f
Christian <thetedioussky@gmail.com>
2026-03-21 19:21:26 -0500

    changed deploy script

--- start.bat
+++ start.bat
@@ -8,6 +8,9 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
+echo Copying config...
+copy /Y "%~dp0plugins\ironkeep-core\src\main\resources\config.yml" "%~dp0server\plugins\IronKeep\config.yml"
+
 echo Starting server...
 cd /d "%~dp0server"
 java -Xms2G -Xmx4G -jar paper-1.21.11-127.jar --nogui
